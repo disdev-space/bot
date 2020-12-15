a53e7f1394b82fbc44289d1d5ccd162c04293534
Marco Ceccon <m.ceccon05@gmail.com>
2020-12-15 19:17:07 +0100

    small bug fix

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -3,11 +3,11 @@ const vars = require("../data/vars");
 const utils = require("../data/utils");
 
 module.exports = (client, message) => {
-        const embed = new Discord.MessageEmbed()
-            .setTitle(`Message deleted!\nOn the channel \`${message.channel.name}\``)
-            .setColor(utils.randomHex())
-            .setDescription(`Message ID: \`${message.id.replace("`", "\`")}`)
-        .addField(`Message content:`, `${message.content.replace("`", "\`")}`)
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`Message deleted!\nOn the channel \`${message.channel.name}\``)
+        .setColor(utils.randomHex())
+        .setDescription(`Message ID: \`${message.id}`)
+        .addField(`Message content:`, `${message.content}`)
         .setAuthor(message.member.user.username, message.member.user.displayAvatarURL({ dynamic: true }))
         .setURL(`https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`)
         .setTimestamp();
