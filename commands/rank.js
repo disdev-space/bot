0b4150c63eb7ae434031f263a389aa86b1d8d72d
Marco Ceccon <m.ceccon05@gmail.com>
2020-12-15 14:48:27 +0100

    Added rank command

---
+++ commands/rank.js
@@ -0,0 +1,17 @@
+const utils = require("../data/utils");
+const Levels = require("discord-xp");
+
+module.exports = {
+    name: 'rank',
+    description: 'Deletes messages.',
+    guildOnly: true,
+    async execute(message, args) {
+        let member = message.mentions.members.first() || message.guild.members.cache.find(user => user.displayName == args[0]) || message.guild.members.cache.find(user => user.id == args[0]) || message.member;
+
+        const user = await Levels.fetch(member.id, message.guild.id);
+
+        if (!user) return message.channel.send(utils.createWarning("Seems like this user has not earned any XP so far."));
+
+        message.channel.send(utils.generateEvent(`**${member.displayName}**'s rank`, `**${member.displayName}** current XP count is **${user.xp}**.\n**${member.displayName}** current level is **${user.level}**.`));
+    },
+};
