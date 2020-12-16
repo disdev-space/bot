c2bf7fe51d3800d6dee197e35dfb1f673c74c22b
Marco Ceccon <m.ceccon05@gmail.com>
2020-12-16 11:42:06 +0100

    Added votes!

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+const Discord = require('discord.js');
+const utils = require("./data/utils");
+
 require('dotenv').config()
-require("./setup")();
+require("./setup")().then((client) => {
+    const express = require('express');
+    const app = express();
+    const PORT = process.env.PORT || 3000;
+    app.use(express.json({ extended: false }));
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.json({ extended: false }));
+    app.post('/votes/end/point', (req, res) => {
+        if (req.body.guild === "788164739850043413") {
+            client.guilds.cache.get("788164739850043413").members.fetch(req.body.user).then((user) => {
+                if (user) {
+                    if (user.user.avatar.startsWith("a_"))
+                        var avatar = `https://images.discordapp.net/avatars/${user.user.id}/${user.user.avatar}.gif`;
+                    else
+                        var avatar = `https://images.discordapp.net/avatars/${user.user.id}/${user.user.avatar}.png`;
+                    const embed = new Discord.MessageEmbed()
+                        .setColor(utils.randomHex())
+                        .setTimestamp()
+                        .setAuthor(user.displayName, avatar)
+                        .setTitle(`**${user.displayName}** just voted!`)
+                        .setDescription(`Thank you ${user.displayName} for voting Code++ on top.gg!\nYou now have the <@&${process.env.VOTE_ROLE}> role, that will give you access to a secret channel!`)
 
-app.post('/votes/end/point', (req, res) => {
-    console.log(req.body);
+                    let role = user.guild.roles.cache.find(r => r.id === process.env.VOTE_ROLE);
+
+                    user.roles.add(role);
+
+                    client.guilds.cache.get("788164739850043413").channels.cache.get("788350810165477406").send(`<@!${user.id}>`, embed);
+                }
+            });
+        }
+    });
+
+    app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 });
-
-app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
--- setup.js
+++ setup.js
@@ -3,38 +3,40 @@ const fs = require('fs');
 const { options } = require("./data/vars");
 const Levels = require("discord-xp");
 
-module.exports = () => {
-    // Levels
-    Levels.setURL(process.env.MONGO_URI);
+module.exports = async() => {
+    return new Promise(resolve => {
+        // Levels
+        Levels.setURL(process.env.MONGO_URI);
 
-    // Create discord.js client
-    const client = new Discord.Client({ restTimeOffset: 200, partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-    // A collection for all the commands
-    client.commands = new Discord.Collection();
+        // Create discord.js client
+        const client = new Discord.Client({ restTimeOffset: 200, partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+        // A collection for all the commands
+        client.commands = new Discord.Collection();
 
-    // Gets all the commands from the "commands" folder
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const command = require(`./commands/${file}`);
-        // Adds commands to commands collection
-        client.commands.set(command.name, command);
-        console.log(`Command loaded: ${file}`);
-    }
+        // Gets all the commands from the "commands" folder
+        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const command = require(`./commands/${file}`);
+            // Adds commands to commands collection
+            client.commands.set(command.name, command);
+            console.log(`Command loaded: ${file}`);
+        }
 
-    // Gets all the events from the "events" folder
-    const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-    for (const file of eventFiles) {
-        const event = require(`./events/${file}`);
-        // Run event
-        client.on(file.replace(".js", ""), event.bind(null, client));
-        console.log(`Event loaded: ${file}`);
-    }
+        // Gets all the events from the "events" folder
+        const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+        for (const file of eventFiles) {
+            const event = require(`./events/${file}`);
+            // Run event
+            client.on(file.replace(".js", ""), event.bind(null, client));
+            console.log(`Event loaded: ${file}`);
+        }
 
-    // Connect to DB
-    options.connectToDB(function(err) {
-        if (err) throw (err);
+        // Connect to DB
+        options.connectToDB(function(err) {
+            if (err) throw (err);
 
-        // Login
-        client.login(process.env.DBT);
+            // Login
+            client.login(process.env.DBT).then(() => resolve(client));
+        });
     });
 };
