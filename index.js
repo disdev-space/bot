b7a9a49a8d5981585031c3cecf81dd56ecb878ce
Marco Ceccon <m.ceccon05@gmail.com>
2020-12-16 10:20:09 +0100

    f

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("./setup")();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json({ extended: false }));
 
 app.post('/votes/end/point', (req, res) => {
     console.log(req.body);
