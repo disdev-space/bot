3f831296a5f6d3476ffdff498e2c10440ec95fad
Marco Ceccon <me@macedon.ga>
2021-04-29 19:17:56 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,10 +3,6 @@ const utils = require("../data/utils");
 
 module.exports = (client, member) => {
     if (!member.user.bot) {
-        let role = member.guild.roles.cache.find(r => r.id === process.env.MEMBER_ROLE);
-
-        member.roles.add(role);
-
         const embed = new Discord.MessageEmbed()
             .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true }))
             .setTitle(`Welcome ${member.displayName}`)
@@ -14,10 +10,6 @@ module.exports = (client, member) => {
             .setDescription(`Welcome ${member.user.username} to Code++!\n• Make sure to read <#788186389853634580> before getting started!\n• Grab yourself some roles at <#788275622711525406>!\n• 10+ Languages Supported!\nYou are our member number ${member.guild.members.cache.filter(member => !member.user.bot).size}!`)
             .setTimestamp();
 
-        client.channels.cache.get(process.env.WELCOME).send(embed);
-    } else {
-        let role = member.guild.roles.cache.find(r => r.id === process.env.BOT_ROLE);
-
-        member.roles.add(role);
+        client.channels.cache.get("837376927072387178").send(embed);
     }
 };
